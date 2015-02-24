7bad677caa3b1a73c826707e9196278fa168b5d7
jens vaaben <jens@jensvaaben.com>
2015-02-24 22:11:03 +0100

    Added filter condition controls. Added file for filter condition implementation.

---
+++ Filter.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace dvbseserviceview
+{
+    internal enum FilterAttributeType
+    {
+        None,
+        Name,
+        Provider,
+        NetworkName,
+        CASystemID,
+        Features
+    }
+
+    internal enum FilterRelationType
+    {
+        None,
+        Is,
+        IsNot,
+        LessThan,
+        MoreThan,
+        BeginsWith,
+        EndsWith,
+        Contains,
+        Excludes,
+        InRange
+    }
+
+    internal class FilterCondition
+    {
+        private FilterAttributeType filterattributetype = FilterAttributeType.None;
+        private FilterRelationType filterrelationtype = FilterRelationType.None;
+        private string value = "";
+
+        public FilterAttributeType filterAttributeType
+        {
+            get
+            {
+                return this.filterattributetype;
+            }
+            set
+            {
+                this.filterattributetype = value;
+            }
+        }
+
+        public FilterRelationType filterRelationType
+        {
+            get
+            {
+                return this.filterrelationtype;
+            }
+            set
+            {
+                this.filterrelationtype = value;
+            }
+        }
+
+        public string Value
+        {
+            get
+            {
+                return this.value;
+            }
+            set
+            {
+                this.value = value;
+            }
+        }
+    }
+}
--- FilterForm.Designer.cs
+++ FilterForm.Designer.cs
@@ -28,19 +28,141 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.radioButtonInclude = new System.Windows.Forms.RadioButton();
+            this.radioButtonExclude = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(31, 108);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(545, 97);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.Location = new System.Drawing.Point(31, 227);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(75, 23);
+            this.buttonOk.TabIndex = 1;
+            this.buttonOk.Text = "&OK";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(31, 38);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(175, 38);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(316, 38);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 4;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(361, 79);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 5;
+            this.buttonAdd.Text = "&Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Location = new System.Drawing.Point(458, 79);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(75, 23);
+            this.buttonRemove.TabIndex = 6;
+            this.buttonRemove.Text = "&Remove";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(31, 79);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(75, 23);
+            this.buttonReset.TabIndex = 7;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonInclude
+            // 
+            this.radioButtonInclude.AutoSize = true;
+            this.radioButtonInclude.Location = new System.Drawing.Point(175, 65);
+            this.radioButtonInclude.Name = "radioButtonInclude";
+            this.radioButtonInclude.Size = new System.Drawing.Size(60, 17);
+            this.radioButtonInclude.TabIndex = 8;
+            this.radioButtonInclude.TabStop = true;
+            this.radioButtonInclude.Text = "&Include";
+            this.radioButtonInclude.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonExclude
+            // 
+            this.radioButtonExclude.AutoSize = true;
+            this.radioButtonExclude.Location = new System.Drawing.Point(175, 84);
+            this.radioButtonExclude.Name = "radioButtonExclude";
+            this.radioButtonExclude.Size = new System.Drawing.Size(63, 17);
+            this.radioButtonExclude.TabIndex = 9;
+            this.radioButtonExclude.TabStop = true;
+            this.radioButtonExclude.Text = "&Exclude";
+            this.radioButtonExclude.UseVisualStyleBackColor = true;
             // 
             // FilterForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(638, 262);
+            this.Controls.Add(this.radioButtonExclude);
+            this.Controls.Add(this.radioButtonInclude);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.buttonOk);
+            this.Controls.Add(this.listView1);
             this.Name = "FilterForm";
             this.Text = "Filter";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.RadioButton radioButtonInclude;
+        private System.Windows.Forms.RadioButton radioButtonExclude;
     }
 }
