fc075fd38551f30b2c43e252ddf3f9525ec9a6d3
jens vaaben <jens@jensvaaben.com>
2015-03-08 11:31:21 +0100

    Implemented filter condition for Name, Provider and NetworkName. Implemented simple filter condition for Features.

--- Filter.cs
+++ Filter.cs
@@ -105,8 +105,155 @@ namespace dvbseserviceview
             listout.Clear();
             foreach (var item in listin)
             {
-                //TODO: apply filter condition
-                listout.Add(item);
+                if (FilterMatch(item, this.FilterConditionSet))
+                {
+                    if (!this.Exclude)
+                    {
+                        listout.Add(item);
+                    }
+                }
+                else
+                {
+                    if (this.Exclude)
+                    {
+                        listout.Add(item);
+                    }
+                }
+
+            }
+        }
+
+        private bool FilterMatch(Service s, SortedSet<FilterCondition> f)
+        {
+            SortedSet<FilterCondition>.Enumerator enumurator = f.GetEnumerator();
+            bool moreitems = enumurator.MoveNext();
+            bool match = false;
+
+            while (moreitems)
+            {
+                // loop through all filter conditions of unique attribute type. Conditions for unique attribute type ared OR'ed.
+                // Groups of unique attribute type are AND'ed.
+                FilterAttributeType filterattributetype = enumurator.Current.filterAttributeType;
+                match = FilterMatch(s, enumurator.Current);
+                while ((moreitems = enumurator.MoveNext()) && filterattributetype == enumurator.Current.filterAttributeType && !match)
+                {
+                    match = FilterMatch(s, enumurator.Current);
+                }
+                // if not at least match for unique attribute type retrurn false. Otherwise proceed with next attribute type
+                if (!match) return false;
+            };
+
+            // if we reached here all groups of unique attribute type AND'ed matched.
+            return true;
+        }
+
+        private bool FilterMatch(Service s, FilterCondition f)
+        {
+            switch (f.filterAttributeType)
+            {
+                case FilterAttributeType.Name:
+                    return FilterMatchName(s,f);
+                case FilterAttributeType.Provider:
+                    return FilterMatchProvider(s,f);
+                case FilterAttributeType.NetworkName:
+                    return FilterMatchNetworkName(s,f);
+                case FilterAttributeType.CASystemID:
+                    return FilterMatchCASystemID(s,f);
+                case FilterAttributeType.Features:
+                    return FilterMatchFeatures(s,f);
+                default:
+                    return false; // this should not happen
+            }
+        }
+
+        private bool FilterMatchName(Service s, FilterCondition f)
+        {
+            switch (f.filterRelationType)
+            {
+                case FilterRelationType.Is:
+                    return s.Name.CompareTo(f.Value)==0;
+                case FilterRelationType.IsNot:
+                    return s.Name.CompareTo(f.Value) != 0;
+                case FilterRelationType.LessThan:
+                    return s.Name.CompareTo(f.Value) < 0;
+                case FilterRelationType.MoreThan:
+                    return s.Name.CompareTo(f.Value) > 0;
+                case FilterRelationType.BeginsWith:
+                    return s.Name.StartsWith(f.Value);
+                case FilterRelationType.EndsWith:
+                    return s.Name.EndsWith(f.Value);
+                case FilterRelationType.Contains:
+                    return s.Name.Contains(f.Value);
+                case FilterRelationType.Excludes:
+                    return !s.Name.Contains(f.Value);
+                default:
+                    return false; // no match for unsupported relation 
+            }
+        }
+
+        private bool FilterMatchProvider(Service s, FilterCondition f)
+        {
+            switch (f.filterRelationType)
+            {
+                case FilterRelationType.Is:
+                    return s.Provider.CompareTo(f.Value) == 0;
+                case FilterRelationType.IsNot:
+                    return s.Provider.CompareTo(f.Value) != 0;
+                case FilterRelationType.LessThan:
+                    return s.Provider.CompareTo(f.Value) < 0;
+                case FilterRelationType.MoreThan:
+                    return s.Provider.CompareTo(f.Value) > 0;
+                case FilterRelationType.BeginsWith:
+                    return s.Provider.StartsWith(f.Value);
+                case FilterRelationType.EndsWith:
+                    return s.Provider.EndsWith(f.Value);
+                case FilterRelationType.Contains:
+                    return s.Provider.Contains(f.Value);
+                case FilterRelationType.Excludes:
+                    return !s.Provider.Contains(f.Value);
+                default:
+                    return false; // no match for unsupported relation 
+            }
+        }
+
+        private bool FilterMatchNetworkName(Service s, FilterCondition f)
+        {
+            switch (f.filterRelationType)
+            {
+                case FilterRelationType.Is:
+                    return s.NetworkName.CompareTo(f.Value) == 0;
+                case FilterRelationType.IsNot:
+                    return s.NetworkName.CompareTo(f.Value) != 0;
+                case FilterRelationType.LessThan:
+                    return s.NetworkName.CompareTo(f.Value) < 0;
+                case FilterRelationType.MoreThan:
+                    return s.NetworkName.CompareTo(f.Value) > 0;
+                case FilterRelationType.BeginsWith:
+                    return s.NetworkName.StartsWith(f.Value);
+                case FilterRelationType.EndsWith:
+                    return s.NetworkName.EndsWith(f.Value);
+                case FilterRelationType.Contains:
+                    return s.NetworkName.Contains(f.Value);
+                case FilterRelationType.Excludes:
+                    return !s.NetworkName.Contains(f.Value);
+                default:
+                    return false; // no match for unsupported relation 
+            }
+        }
+
+        private bool FilterMatchCASystemID(Service s, FilterCondition f)
+        {
+            return true;
+        }
+
+        private bool FilterMatchFeatures(Service s, FilterCondition f)
+        {
+            switch (f.filterRelationType)
+            {
+                case FilterRelationType.Contains:
+                    return s.FeatureList.Contains(f.Value);
+                default:
+                    return false; // no match for unsupported relation 
             }
         }
     }
