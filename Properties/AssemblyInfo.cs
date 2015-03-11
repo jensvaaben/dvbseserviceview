9309b39b074eba817019a5f60b67e8798d4ee758
jens vaaben <jens@jensvaaben.com>
2015-03-11 23:03:37 +0100

    Updated copyright year.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jens Vaaben")]
 [assembly: AssemblyProduct("DVBStreamExplorer Service Viewer")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2014-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
