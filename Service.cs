7da430d54ed0713781c274bd98e9f10383e63c95
jens vaaben <jens@jensvaaben.com>
2015-04-28 21:23:07 +0200

    Added support for OC, DC and unspecified carousel.

--- Service.cs
+++ Service.cs
@@ -769,7 +769,7 @@ namespace dvbseserviceview
             SortedSet<string> list = new SortedSet<string>();
             foreach (var s in this.streams)
             {
-                if (s.Type2 == "mhp" || s.Type2 == "ip" || s.Type2 == "mhp_oc" || s.Type2 == "mpe" || s.Type2 == "hbbtv" || s.Type2 == "ssu")
+                if (s.Type2 == "mhp" || s.Type2 == "ip" || s.Type2 == "mhp_oc" || s.Type2 == "mpe" || s.Type2 == "hbbtv" || s.Type2 == "ssu" || s.Type2 == "object_carousel" || s.Type2 == "data_carousel" || s.Type2 == "carousel")
                 {
                     list.Add(s.Type2);
                 }
